e27b91024ec495c938a318c66abd93e1a47b74fa
Anshulranjan845 <anshulgulati177@gmail.com>
2026-05-15 16:52:07 +0530

    route fix

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,40 +3,24 @@ import { Route, Routes } from "react-router-dom";
 
 import LoginPage from "../features/auth/pages/LoginPage";
 import RegisterPage from "../features/auth/pages/RegisterPage";
-import CartPage from "../features/cart/pages/CartPage";
-import CheckoutPage from "../features/checkout/pages/CheckoutPage";
-import AdminHomePage from "../features/admin/pages/AdminHomePage";
-import AdminProductsPage from "../features/admin/pages/AdminProductsPage";
-import CreateProductPage from "../features/admin/pages/CreateProductPage";
+
 import ProductDetailPage from "../features/products/pages/ProductDetailPage";
 
+import CartPage from "../features/cart/pages/CartPage";
+import CheckoutPage from "../features/checkout/pages/CheckoutPage";
+
 import HomePage from "../pages/HomePage";
+import PaymentSuccessPage from "../pages/PaymentSuccessPage";
+import PaymentFailedPage from "../pages/PaymentFailedPage";
+import UnauthorizedPage from "../pages/UnauthorizedPage";
 
 import ProtectedRoute from "./ProtectedRoute";
 import PublicOnlyRoute from "./PublicOnlyRoute";
 import AdminRoute from "./AdminRoute";
-
-import ProductDetailPage from "../features/products/pages/ProductDetailPage";
-import CartPage from "../features/cart/pages/CartPage";
-import CheckoutPage from "../features/checkout/pages/CheckoutPage";
-
-import PaymentSuccessPage from "../pages/PaymentSuccessPage";
-import PaymentFailedPage from "../pages/PaymentFailedPage";
-import UnauthorizedPage from "../pages/UnauthorizedPage";
 
 import MainLayout from "../layouts/MainLayout";
 import AdminLayout from "../layouts/AdminLayout";
 
-import HomePage from "../pages/HomePage";
-import PaymentSuccessPage from "../pages/PaymentSuccessPage";
-import PaymentFailedPage from "../pages/PaymentFailedPage";
-import UnauthorizedPage from "../pages/UnauthorizedPage";
-
-import ProtectedRoute from "./ProtectedRoute";
-import PublicOnlyRoute from "./PublicOnlyRoute";
-import AdminRoute from "./AdminRoute";
-
-const ProductsPage = lazy(() => import("../features/products/pages/ProductsPage"));
 import CreateProductPage from "../features/admin/pages/CreateProductPage";
 import AdminHomePage from "../features/admin/pages/AdminHomePage";
 import AdminProductsPage from "../features/admin/pages/AdminProductsPage";
@@ -48,7 +32,7 @@ const ProductsPage = lazy(
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* Main Layout Routes */}
+      {/* MAIN ROUTES */}
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
 
@@ -99,35 +83,14 @@ export default function AppRoutes() {
           }
         />
 
-        <Route path="unauthorized" element={<UnauthorizedPage />} />
-        <Route path="payment-success" element={<PaymentSuccessPage />} />
-
-        <Route
-          path="login"
-          element={
-            <PublicOnlyRoute>
-              <LoginPage />
-            </PublicOnlyRoute>
-          }
-        />
-
-        <Route
-          path="register"
-          element={
-            <PublicOnlyRoute>
-              <RegisterPage />
-            </PublicOnlyRoute>
-          }
-        />
-
-        <Route path="unauthorized" element={<UnauthorizedPage />} />
-
         <Route path="payment-success" element={<PaymentSuccessPage />} />
 
         <Route path="payment-failed" element={<PaymentFailedPage />} />
+
+        <Route path="unauthorized" element={<UnauthorizedPage />} />
       </Route>
 
-      {/* Admin Routes */}
+      {/* ADMIN ROUTES */}
       <Route
         path="/admin"
         element={
@@ -139,7 +102,9 @@ export default function AppRoutes() {
         }
       >
         <Route index element={<AdminHomePage />} />
+
         <Route path="products" element={<AdminProductsPage />} />
+
         <Route path="products/create" element={<CreateProductPage />} />
       </Route>
     </Routes>
