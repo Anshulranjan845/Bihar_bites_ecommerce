981d0c544bc3ec36a220aa53c870a755639f5195
Anshulranjan845 <anshulgulati177@gmail.com>
2026-05-16 12:10:08 +0530

    category patch

--- client/src/features/admin/pages/CategoryPage.jsx
+++ client/src/features/admin/pages/CategoryPage.jsx
@@ -18,7 +18,8 @@ export default function CategoryPage() {
     try {
       setLoading(true);
       const res = await getCategories();
-      setCategories(res.data || []);
+
+      setCategories((res.data || []).filter((cat) => !cat.isDeleted));
     } catch {
       toast.error("Failed to load categories");
     } finally {
