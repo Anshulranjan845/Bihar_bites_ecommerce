7fc95970ed5008d7de6b679df2312e288bd3daff
Anshulranjan845 <anshulgulati177@gmail.com>
2026-05-17 16:47:48 +0530

    ..

--- server/src/app.js
+++ server/src/app.js
@@ -13,12 +13,20 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://bihar-bites-ecommerce-nvc3.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://bihar-bites-ecommerce-nvc3.vercel.app/",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
